01d08e3f09761769c6684237c55531a807fd50f6
unknown <sohantipre01@gmail.com>
2021-09-02 12:08:13 +0530

    added final screens

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import connect from "./config/db.js";
 import Userroute from "./routes/userroute.js";
 import Productroute from "./routes/productroute.js";
+import Orderroute from "./routes/orderroute.js";
+
 const app = express();
 
 connect();
@@ -9,6 +11,7 @@ connect();
 app.use(express.json());
 app.use("/api/users", Userroute);
 app.use("/api/products", Productroute);
+app.use("/api/orders", Orderroute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
